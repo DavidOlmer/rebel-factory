22a1272eb432c664e11f8420267630cd4ed15b85
DavidOlmer <david.olmer@rebelgroup.com>
2026-03-23 17:52:50 +0100

    🎨 Admin UI: index.ts

---
+++ frontend/src/pages/admin/index.ts
@@ -0,0 +1,8 @@
+// Admin Pages
+export { default as AdminDashboard } from './Dashboard'
+export { default as AdminUsers } from './Users'
+export { default as AdminTenants } from './Tenants'
+export { default as AdminAgents } from './Agents'
+export { default as AdminAnalytics } from './Analytics'
+export { default as AdminAuditLogs } from './AuditLogs'
+export { default as AdminSecurity } from './Security'
