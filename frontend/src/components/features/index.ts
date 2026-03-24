7d762435a554f2a79a5fdc71e7d26ab59684d471
DavidOlmer <david.olmer@rebelgroup.com>
2026-03-24 10:19:38 +0100

    🎨 Design: index.ts

---
+++ frontend/src/components/features/index.ts
@@ -0,0 +1,5 @@
+export { AgentCard } from './AgentCard';
+export { TemplateCard } from './TemplateCard';
+export { PromptCard } from './PromptCard';
+export { InsightAlert } from './InsightAlert';
+export { ApprovalItem } from './ApprovalItem';
